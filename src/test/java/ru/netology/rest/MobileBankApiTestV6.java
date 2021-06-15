61e5b2f786f60162eaabd82f076a145de94af0ba
Alexander Siluyanov <antidot8@mail.ru>
2021-06-15 21:41:21 +0400

    Build success

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -30,7 +30,7 @@ class MobileBankApiTestV6 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
